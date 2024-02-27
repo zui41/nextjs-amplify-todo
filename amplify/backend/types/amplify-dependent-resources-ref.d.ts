47959d195ec269cf74733d3d52970724a4472280
zui41 <zui19990401@gmail.com>
2024-02-28 08:42:04 +0900

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "todoapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "todoapp9948c251": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,173 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import React from 'react';
+import {
+  Box,
+  Stack,
+  TextField,
+  Typography,
+  Container,
+  Paper,
+  Button,
+  Snackbar,
+  Alert,
+  Slide,
+} from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useForm, FormProvider } from 'react-hook-form';
+import * as Yup from "yup";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { generateClient } from 'aws-amplify/api';
+import { createTodo, deleteTodo } from '../graphql/mutations';
+import { listTodos } from '../graphql/queries';
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from '../amplifyconfiguration.json';
+import { CreateTodoInput, Todo } from '../API';
+import { withAuthenticator, WithAuthenticatorProps } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(amplifyconfig);
+const client = generateClient();
+
+const Todo = ({ signOut, user}: WithAuthenticatorProps) => {
+  const [todos, setTodos] = useState<Todo[] | CreateTodoInput[]>([]);
+
+  const defaultValues = {
+    name: "",
+    description: "",
+  };
+  const validation = Yup.object().shape({
+    name: Yup.string().required("タスク名を入力してください。"),
+    description: Yup.string(),
+  });
+  const methods = useForm({
+    defaultValues,
+    resolver: yupResolver(validation),
+  });
+  const { handleSubmit, register, resetField, formState } = methods;
+  const { errors } = formState;
+
+  const fetchTodo = async () => {
+    try {
+      const todoData = await client.graphql({
+        query: listTodos,
+        variables: { filter: { userId: { eq: user?.userId } } },
+      });
+      const todos = todoData.data.listTodos.items;
+      setTodos(todos);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const addTodo = async (data: Omit<CreateTodoInput, "userId">) => {
+    try {
+      const todoData = { ...data, userId: user?.userId ?? "" };
+      await client.graphql({
+        query: createTodo,
+        variables: { input: todoData },
+      });
+      resetField("name");
+      resetField("description");
+      await fetchTodo();
+    } catch (error) {
+      console.log(error); 
+    }
+  };
+
+  const handleDeleteTodo = async (id: string) => {
+    try {
+      await client.graphql({
+        query: deleteTodo,
+        variables: { input: { id } },
+      });
+      await fetchTodo();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTodo();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Box>
+      <Container sx={{ p: '4vh' }}>
+        <Typography variant="h4">
+          TO DO
+        </Typography>
+        <Box textAlign="right" sx={{ mb: "1vh" }}>
+          <Button variant="contained" onClick={signOut}>
+          Sign Out
+          </Button>
+        </Box>
+        <Paper elevation={6} sx={{ p: '2vh' }}>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          { /* Add Task */ }
+          <FormProvider {...methods}>
+            <form onSubmit={methods.handleSubmit(addTodo)}>
+              <Stack mb={2}>
+                <TextField
+                  label="タスク名"
+                  rows={1}
+                  {...register("name")} 
+                />
+                {errors.name && (
+                  <Typography variant="caption" color="error">
+                    {errors.name.message}
+                  </Typography>
+                )}
+              </Stack>
+              <Stack mb={2}>
+                <TextField
+                  label="説明"
+                  multiline
+                  rows={4}
+                  {...register("description")} 
+                />
+              </Stack>
+              <Box textAlign="right">
+                <Button type="submit" variant="outlined">追加</Button>
+              </Box>
+            </form>
+          </FormProvider>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          { /* Todo List */ }
+          {todos.map((todo) => (
+            <Paper elevation={3} sx={{ p: "2vh", width: "100%", mt: "4vh" }}>
+                <Stack direction="row" justifyContent="space-between">
+                  <Typography variant="h6">
+                    {todo.name}
+                  </Typography>
+                </Stack>
+                <Stack direction="row" justifyContent="space-between">
+                  <Typography sx={{ textAlign: 'center', lineHeight: '2.5' }}>
+                    {todo.description}
+                  </Typography>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                  <Stack direction="row" justifyContent="flex-end">
+                    <Button
+                      variant="text"
+                      color="error"
+                      onClick={() => {
+                        if (typeof todo?.id === "string") {
+                          handleDeleteTodo(todo.id)
+                        }
+                      }}
+                    >
+                      完了
+                    </Button>
+                  </Stack>
+                </Stack>
+            </Paper>
+          ))}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        </Paper>
+      </Container>
+    </Box>
   );
 }
+
+export default withAuthenticator(Todo);
